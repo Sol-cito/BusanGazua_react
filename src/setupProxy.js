b793e37228e699bfcab06e88413a6541ad26f4a6
dataenggu <dsolkim1030@gmail.com>
2021-03-08 20:31:28 +0900

    edit proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,6 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app){
   app.use(
       createProxyMiddleware('/api', {
+        //   target: 'http://localhost:4000',
           target: 'http://3.22.68.53:4000',
           changeOrigin: true
       })
